abc38aa74cca509f627dfb19fce3fda86cead4cf
ursrBaek <ursrbaek@gmail.com>
2023-12-05 12:06:20 +0900

    feat: login 페이지 추가 및 입력란 체크 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '백나라' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '백나라' });
 });
 
 module.exports = router;
